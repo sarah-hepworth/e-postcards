bd512fe83fb7d2d3035f0406d34b7474239a47e7
Sarah Hepworth <sarah.hepworth@bunches.co.uk>
2023-11-10 14:52:49 +0000

    add initial navigation styles

--- src/app/ui/navigation/Navigation.tsx
+++ src/app/ui/navigation/Navigation.tsx
@@ -1,5 +1,20 @@
 import { FC, ReactElement } from "react";
+import styles from "@/app/navigation.module.css";
 
 export const Navigation: FC = (): ReactElement => {
-  return <p>I am the navigation</p>;
+  return (
+    <div className={styles.navigation}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Jellywish</h1>
+        <span>Postcards delivered straight to their mailbox</span>
+      </div>
+
+      <div className={styles.items}>
+        <div className={styles.item}>Postcards</div>
+        <div className={styles.item}>Personalise your own</div>
+        <div className={styles.item}>Holiday photobooks</div>
+        <div className={styles.item}>Group postcards</div>
+      </div>
+    </div>
+  );
 };
