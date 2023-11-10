1ff18b7cc965c3ffb64311a1a20c4e7d39d12252
Sarah Hepworth <sarah.hepworth@bunches.co.uk>
2023-11-10 11:03:59 +0000

    add brand colour variables

---
+++ src/app/product/page.tsx
@@ -0,0 +1,6 @@
+import styles from "@/app/page.module.css";
+import { ReactElement } from "react";
+
+export default function Page() {
+  return <main className={styles.main}>hello I am the product page</main>;
+}
