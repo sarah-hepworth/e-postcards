ece42fc01ccad0b5ec7a10fa2b8a2f12d971a683
Sarah Hepworth <sarah.hepworth@bunches.co.uk>
2023-12-15 11:40:32 +0000

    convert hero to tailwind

--- src/app/ui/hero/hero.tsx
+++ src/app/ui/hero/hero.tsx
@@ -1,24 +1,20 @@
 import { FC, ReactElement } from "react";
-import styles from "@/app/styles/hero.module.css";
-import Image from "next/image";
 
 export const Hero: FC = (): ReactElement => {
   return (
-    <div className={styles.background}>
-      <div className={`container mx-auto ${styles.container}`}>
-        <Image src="/hero-postcard-stock-image.jpg" alt="Hero stock image" width="1000" height="1000" />
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>Postcards delivered</h1>
+    <div className="container mx-auto grid lg:grid-cols-2 lg:mt-4 text-gray-800">
+      <div className="h-56 bg-[url('/hero-postcard-stock-image.jpg')] bg-top bg-cover lg:h-full"></div>
+      <div className="flex flex-col justify-center items-center p-6 bg-indigo-200">
+        <h1 className="text-4xl font-bold text-center mb-4">Postcards delivered</h1>
 
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec sodales leo. Vivamus pretium velit
-            enim, eu aliquet nibh maximus et. Pellentesque in elit nisi. Morbi et sapien a mi finibus luctus. Donec
-            maximus orci sit amet pretium maximus. Vestibulum pretium, metus eget rutrum consequat, tortor ex lacinia
-            orci, sit amet convallis dui dui vel nulla. I
-          </p>
+        <p className="leading-8 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec sodales leo. Vivamus pretium velit
+          enim, eu aliquet nibh maximus et. Pellentesque in elit nisi. Morbi et sapien a mi finibus luctus. Donec
+          maximus orci sit amet pretium maximus. Vestibulum pretium, metus eget rutrum consequat, tortor ex lacinia
+          orci, sit amet convallis dui dui vel nulla. I
+        </p>
 
-          <button className={styles.button}>Send Now</button>
-        </div>
+        <button className="bg-white py-2 py 1 text-gray-800 rounded-sm w-48 mt-4">Send Now</button>
       </div>
     </div>
   );
